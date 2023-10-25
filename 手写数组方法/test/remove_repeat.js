5d9a6cddb8e8860f322dec67dbe48aece3b1f0e1
Red (Hongru) Cai <hongru.cai@iglooinsure.com>
2023-10-25 20:26:11 +0800

    二维数组的交集remove-repeat.js

--- 手写数组方法/test/remove_repeat.js
+++ 手写数组方法/test/remove_repeat.js
@@ -24,4 +24,14 @@ function removeRepeatInArray(array){
   return res
 }
 
+// 二维数组求交集
+// [[1,2,3,5], [2,4,5,3], [8,3,5]...] => [5]
+function arrayIntersection(arr){
+  return arr.reduce((res, curr) => {
+    console.log(curr.filter(v => res.includes(v)))
+    return curr.filter(v => res.includes(v));
+  })
+}
+
 console.log(removeRepeatInArray([1,2,3,1,5,15,16,8,8,9,0,2]))
+console.log(arrayIntersection([[1,2,3,5], [2,4,5,3], [8,3,5]]))
